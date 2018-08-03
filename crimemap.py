fcd23dc7cd850a30d1435fb726a5ef1a7ac20ce6
Roger <birde1618@gmail.com>
2018-08-03 16:03:38 -0300

    Map with form and CSS

--- crimemap.py
+++ crimemap.py
@@ -1,7 +1,12 @@
-from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+import dbconfig
+
+if dbconfig.test:
+    from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 
 app = Flask(__name__)
 DB = DBHelper()
@@ -11,7 +16,7 @@ def home():
     try:
         data = DB.get_all_inputs()
     except Exception as e:
-        print e
+        print (e)
         data = None
     return render_template("home.html", data=data)
 
@@ -21,7 +26,7 @@ def add():
         data = request.form.get("userinput")
         DB.add_input(data)
     except Exception as e:
-        print e
+        print (e)
     return home()
 
 @app.route("/clear")
@@ -29,7 +34,7 @@ def clear():
     try:
         DB.clear_all()
     except Exception as e:
-        print e
+        print (e)
     return home()
 
 if __name__ == '__main__':
