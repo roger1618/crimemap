1917a634cd56700586fcf15076ca5e76f3e5e499
Roger <birde1618@gmail.com>
2018-08-05 17:24:13 -0300

    add new crimes functionality

--- dbhelper.py
+++ dbhelper.py
@@ -1,6 +1,6 @@
 import pymysql
 import dbconfig
-
+import datetime
 class DBHelper:
     def connect(self, database="crimemap"):
         return pymysql.connect(host='localhost',
@@ -45,5 +45,23 @@ class DBHelper:
                 connection.commit()
         except Exception as e:
             print(e)
+        finally:
+            connection.close()
+    def get_all_crimes(self):
+        connection = self.connect()
+        try:
+            query = "SELECT latitude, longitude, date, category, description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            named_crimes = []
+            for crime in cursor:
+                named_crime = {'latitude': crime[0], 
+                        'longitude': crime[1],
+                        'date': datetime.datetime.strftime(crime[2],
+                        '%Y-%m-%d'),
+                        'category': crime[3],
+                        'description': crime[4]}
+                named_crimes.append(named_crime)
+            return named_crimes
         finally:
             connection.close()
